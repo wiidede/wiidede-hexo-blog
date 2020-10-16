be2359bb7db92eaf1495c554f71948be68c31be6
wiidede <wiixdede@gmail.com>
2020-10-16 17:04:46 +0800

    fix console error

--- themes/ayer/source-src/js/ayer.js
+++ themes/ayer/source-src/js/ayer.js
@@ -162,16 +162,16 @@
     'background-color: #ffbca2; padding: 8px; font-size: 14px;';
   const ayerNameStyle = 'background-color: #eaf8ff;';
 
-  console.log(
-    '%c%s%c%s%c%s',
-    ayerInfoStyle,
-    ayerInfo,
-    ayerURLStyle,
-    ayerURL,
-    ayerNameStyle,
-    ayerNameStr
-  );
-  document.write('<script type="text/javascript"  src="https://js.users.51.la/20544303.js"></script>');
+  // console.log(
+  //   '%c%s%c%s%c%s',
+  //   ayerInfoStyle,
+  //   ayerInfo,
+  //   ayerURLStyle,
+  //   ayerURL,
+  //   ayerNameStyle,
+  //   ayerNameStr
+  // );
+  // document.write('<script type="text/javascript"  src="https://js.users.51.la/20544303.js"></script>');
 })(jQuery);
 
 
